960dc326609e88f778f5969ff5afa30e1bdaaea2
kensuke yoshida <kenban03@gmail.com>
2017-06-27 22:11:21 +0800

    初めてのmyAppleのコミット

---
+++ myApple01/FirstViewController.swift
@@ -0,0 +1,28 @@
+//
+//  FirstViewController.swift
+//  myApple01
+//
+//  Created by kensuke yoshida on 2017/06/27.
+//  Copyright © 2017年 kensuke yoshida. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+    
+    @IBOutlet weak var myFrame: UITextField!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the                    view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
