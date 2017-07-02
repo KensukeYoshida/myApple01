adbf7e7602d7f4fbe81a166a910adaa40562670a
kensuke yoshida <kenban03@gmail.com>
2017-07-03 02:15:33 +0800

    coredata追加前

--- myApple01/FirstViewController.swift
+++ myApple01/FirstViewController.swift
@@ -7,16 +7,47 @@
 //
 
 import UIKit
+import CoreData
 
 class FirstViewController: UIViewController {
     
+    @IBOutlet weak var txtTitle: UILabel!
     
-    @IBOutlet weak var myFrame: UILabel!
+    @IBOutlet weak var txtTitle2: UILabel!
+    
+    @IBOutlet weak var txtTitle3: UILabel!
     
 
+    override func viewWillAppear(_ animated: Bool) {
+        
+        var words = ["辞書","Bluetooth","ソーラー","財布","電卓","ペン","人工知能","オークション","ドローン","VR","シンセサイザー","ライト","ペン","机","イス","車","コップ","メガネ"]
+        
+        let r = Int(arc4random()) % words.count
+        let s = Int(arc4random()) % words.count
+        let t = Int(arc4random()) % words.count
+        
+        txtTitle.text = words[r] as! String
+        
+        txtTitle2.text = words[s] as! String
+
+        txtTitle3.text = words[t] as! String
+
+    
+    }
+    
+   
+    
+//
+//    switch r {
+//        case 0:
+//        txtTitle.text = UILabel
+//    
+//    }
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the                    view, typically from a nib.
+      
     }
     
     //メモリ不足のとき表示
--- myApple01/SecondViewController.swift
+++ myApple01/SecondViewController.swift
@@ -7,13 +7,57 @@
 //
 
 import UIKit
+import CoreData
 
-class SecondViewController: UIViewController {
+class SecondViewController: UIViewController{
+    
 
+    @IBOutlet weak var txtTitle: UITextField!
+    
+     var words = [""]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    
+    @IBAction func tapReturn(_ sender: UITextField) {
+    }
+    
+    //OKボタン押された時、CoreDate追加
+    @IBAction func tapSave(_ sender: UIButton) {
+       
+        //AppDelegateのインスタンスを用意しておく (as!はダウンキャスト ＜値＞as!＜型＞)
+        let appDelegate:AppDelegate = UIApplication.shared.delegate as! AppDelegate
+        
+        //エンティティを操作するためのオブジェクトを作成
+        let viewContext = appDelegate.persistentContainer.viewContext
+        
+        //Wordsエンティティオブジェクトを作成
+        let Words = NSEntityDescription.entity(forEntityName: "Words", in: viewContext)
+        
+        //Wordsエンティティにレコード(行)を挿入するためのオブジェクトを作成
+        let newRecord = NSManagedObject(entity: Words!, insertInto: viewContext)
+        //③上記まで
+        
+        
+        //追加したいdata(txtTitleに入力された文字)のセット
+        newRecord.setValue(txtTitle.text, forKey: "title")//値の代入
+        newRecord.setValue(Date(),forKey: "saveDate")
+        //Date():現在日時がセットできる
+        
+            
+        
+        //レコード（行）の即時保存
+        do{
+            try viewContext.save()
+        }catch{
+        }
+        
+        
+    }
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
