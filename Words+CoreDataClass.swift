e1f1039dd1be0c0a0452381b51605a3d6a8d58bc
kensuke yoshida <kenban03@gmail.com>
2017-07-03 02:15:05 +0800

    coredata追加前

---
+++ Words+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Words+CoreDataClass.swift
+//  myApple01
+//
+//  Created by kensuke yoshida on 2017/06/30.
+//  Copyright © 2017年 kensuke yoshida. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+
+public class Words: NSManagedObject {
+
+}
---
+++ Words+CoreDataProperties.swift
@@ -0,0 +1,22 @@
+//
+//  Words+CoreDataProperties.swift
+//  myApple01
+//
+//  Created by kensuke yoshida on 2017/07/02.
+//  Copyright © 2017年 kensuke yoshida. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+
+extension Words {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<Words> {
+        return NSFetchRequest<Words>(entityName: "Words")
+    }
+
+    @NSManaged public var saveDate: NSDate?
+    @NSManaged public var title: String?
+
+}
